3d8886b45e2e7eb6ed0bfd0a8d3c628877a749b1
kunalsahu <kunalsahu071@gmail.com>
2024-10-02 22:03:34 +0530

    changesnew

--- config.py
+++ config.py
@@ -1,19 +1,19 @@
 from pymongo.mongo_client import MongoClient
 import urllib
 
-usernmae = urllib.parse.quote("goodghost340")
-passwd = urllib.parse.quote("riot@vky342")
+usernmae = urllib.parse.quote("kunalsahu071")
+passwd = urllib.parse.quote("root@vky342")
 
-uri = "mongodb+srv://%s:%s@blogcust.yl7ag.mongodb.net/?retryWrites=true&w=majority&appName=blogcust&ssl=true&ssl_cert_reqs=CERT_NONE" % (usernmae, passwd)
+uri = "mongodb+srv://%s:%s@confessiontest.dpwqu.mongodb.net/?retryWrites=true&w=majority&appName=confessiontest&ssl=true&ssl_cert_reqs=CERT_NONE" % (usernmae, passwd)
 
 # Create a new client and connect to the server
 client = MongoClient(uri)
-db = client["blogcust"]
-mycollection = db["CONTENTS"]
+db = client["confessiontest"]
+mycollection = db["Submits"]
 
 # Send a ping to confirm a successful connection
 try:
     client.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    print("Pinged your deployment. You successfully connected to MongoDB! confession test")
 except Exception as e:
     print(e)
--- main.py
+++ main.py
@@ -10,14 +10,19 @@ app = FastAPI()
 async def receive_data(request: Request):
     try:
         data = await request.json()
-        if "time" not in data or "content" not in data:
-            raise HTTPException(status_code=400, detail="Missing required fields: time and content")
+        if "time" not in data or "content" not in data or "comments" not in data:
+            raise HTTPException(status_code=400, detail="Missing required fields: time, content, and comments")
+
+        # Add default empty list for comments if not provided
+        if not data["comments"]:
+            data["comments"] = []
 
         result = mycollection.insert_one(data)
         return {"message": "Data inserted successfully", "inserted_id": str(result.inserted_id)}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
-    
+
+
 @app.get("/retrieve_data")
 async def retrieve_data():
     try:
@@ -26,6 +31,7 @@ async def retrieve_data():
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World 3"}
